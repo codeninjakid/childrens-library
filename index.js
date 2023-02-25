db265b5dedf593817969974599d5195301be54f7
Yaksh Shah <yakshrshah@gmail.com>
2023-02-24 17:56:26 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// var canvas = document.querySelector('#gameboard');
+// var ctx = canvas.getContext("2d");
+
+// ctx.lineWidth = 5;
+// ctx.fillStyle = 'rgb(255, 178, 51)';
+// ctx.fillRect(0, 0, 500, 500);
+
+// ctx.font = "50px serif";
+// ctx.fillStyle = 'rgb(0, 0, 0)';
+// ctx.fillText("   Start", 20, 100);
+// ctx.fillText(' reading', 120, 200);
+// ctx.fillText('!!!!!!!!!', 220, 300);
